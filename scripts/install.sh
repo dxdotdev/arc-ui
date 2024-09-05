a5de95ae6c10e061952781e3a273419d5a381fee
Davi Reis <dreisss.dev@gmail.com>
2024-09-05 20:38:49 -0300

    fix(scripts/install): fix error on not existing "chrome" folder or "user.js" and "userChrome.css" files

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,9 @@ user=$(grep 'Path' ~/.mozilla/firefox/profiles.ini | sed 's/Path=//g')
 userSettings=~/.mozilla/firefox/$user/user.js
 userChromeCSSPath=~/.mozilla/firefox/$user/chrome/userChrome.css
 
+mkdir -p "$HOME/.mozilla/firefox/$user/chrome"
+touch "$userChromeCSSPath"
+
 curl -L https://github.com/dxdotdev/arc-ui-theme/releases/latest/download/userChrome.css >"$userChromeCSSPath"
 
 if [ ! -e "$userSettings" ] || (! grep 'toolkit.legacyUserProfileCustomizations.stylesheets' "$userSettings"); then
